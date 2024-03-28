4512c9357765202a90e967c4ea4d986d6ff18fb7
Ana Garcia <ana.garcia30@utrgv.edu>
2024-03-28 09:11:06 -0500

    Set up PHP email handling

--- mail.php
+++ mail.php
@@ -1,14 +1,47 @@
 <?php
-  if(isset( $_POST['name']))
-  $name = $_POST['name'];
-  if(isset( $_POST['email']))
-  $email = $_POST['email'];
-  if(isset( $_POST['message']))
-  $message = $_POST['message'];
+$errors = [];
+$errorMessage = '';
 
-  $content="From: $name \n Email: $email \n Message: $message";
-  $recipient = "youremail@here.com";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $content, $mailheader) or die("Error!");
-  echo "Email sent!";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Validate inputs
+    if (empty($name)) {
+        $errors[] = 'Name is required';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is required';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is required';
+    }
+
+    if (empty($errors)) {
+        // Send email
+        $toEmail = 'michellana.garcia@gmail.com'; // Replace with your email address
+        $emailSubject = 'New email from your contact form';
+        $headers = "From: $email\r\nReply-To: $email\r\nContent-type: text/html; charset=utf-8";
+        $body = "Name: $name<br>Email: $email<br>Message: $message";
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+            exit(); // Always exit after header redirect
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $errorMessage = "<div style='color: red;'>";
+        $errorMessage .= "<p>Please correct the following errors:</p><ul>";
+        foreach ($errors as $error) {
+            $errorMessage .= "<li>$error</li>";
+        }
+        $errorMessage .= "</ul></div>";
+    }
+}
 ?>
